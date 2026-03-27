1d72bdc4c6b5d838954079dc1469f01efecbc6fc
apexdegen <159210398+apexdegen@users.noreply.github.com>
2026-03-27 16:54:14 +1100

    feat: add SimpleHelper.sol

---
+++ scripts/contracts/SimpleHelper.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SimpleHelper {
+    function getVersion() public pure returns (string memory) {
+        return "v1.0 - Base ERC-6551 Helper";
+    }
+}
